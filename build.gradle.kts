e4e35cbca78cd732859f28cf54557ca70219c417
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-05-24 18:19:46 +0100

    recovery post deletion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jooq.meta.jaxb.Logging
+
 plugins {
     id("marine.application-conventions")
 }
@@ -8,6 +10,51 @@ dependencies {
     testImplementation(project(":test"))
     runtimeOnly("org.liquibase:liquibase-core")
     runtimeOnly("com.h2database:h2")
+}
+
+jooq {
+    configuration {
+        logging = Logging.TRACE
+        generator {
+            database {
+                name = "org.jooq.meta.extensions.liquibase.LiquibaseDatabase"
+                properties {
+                    // Specify the classpath location of your XML, YAML, or JSON script.
+                    property {
+                        key = "scripts"
+                        value = "config/liquibase/master.yaml"
+                    }
+                    // Whether you want to include liquibase tables in generated output
+                    property {
+                        key = "includeLiquibaseTables"
+                        value = "false"
+                    }
+                    // Properties prefixed "database." will be passed on to the liquibase.database.Database class
+                    // if a matching setter is found
+                    property {
+                        key = "database.liquibaseSchemaName"
+                        value = "lb"
+                    }
+                    // The property "changeLogParameters.contexts" will be passed on to the
+                    // liquibase.database.Database.update() call (jOOQ 3.13.2+).
+                    // See https://www.liquibase.org/documentation/contexts.html
+                    property {
+                        key = "changeLogParameters.contexts"
+                        value = "ddl"
+                    }
+                }
+            }
+            generate {
+                isFluentSetters = true
+                isJavaTimeTypes = true
+            }
+            target {
+                packageName = "uk,co.bluegecko.marine.vessel.data.jooq"
+                directory = "src/main/generated"
+                isClean = true
+            }
+        }
+    }
 }
 
 testing {
