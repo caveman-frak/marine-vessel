4051a8302765fc48c810465e56e051c781c4b372
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-05-28 07:46:47 +0100

    enabled querydsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jooq.meta.jaxb.Logging
-
 plugins {
     id("marine.application-conventions")
 }
@@ -7,51 +5,6 @@ plugins {
 dependencies {
     implementation(project(":shared"))
     implementation(project(":wire"))
-}
-
-jooq {
-    configuration {
-        logging = Logging.TRACE
-        generator {
-            database {
-                name = "org.jooq.meta.extensions.liquibase.LiquibaseDatabase"
-                properties {
-                    // Specify the classpath location of your XML, YAML, or JSON script.
-                    property {
-                        key = "scripts"
-                        value = "config/liquibase/master.yaml"
-                    }
-                    // Whether you want to include liquibase tables in generated output
-                    property {
-                        key = "includeLiquibaseTables"
-                        value = "false"
-                    }
-                    // Properties prefixed "database." will be passed on to the liquibase.database.Database class
-                    // if a matching setter is found
-                    property {
-                        key = "database.liquibaseSchemaName"
-                        value = "lb"
-                    }
-                    // The property "changeLogParameters.contexts" will be passed on to the
-                    // liquibase.database.Database.update() call (jOOQ 3.13.2+).
-                    // See https://www.liquibase.org/documentation/contexts.html
-                    property {
-                        key = "changeLogParameters.contexts"
-                        value = "ddl"
-                    }
-                }
-            }
-            generate {
-                isFluentSetters = true
-                isJavaTimeTypes = true
-            }
-            target {
-                packageName = "uk,co.bluegecko.marine.vessel.data.jooq"
-                directory = "src/main/generated"
-                isClean = true
-            }
-        }
-    }
 }
 
 testing {
