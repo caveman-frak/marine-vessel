ac18546e8cd99f4713eb36a46ff901cf1e6be85e
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-06-18 10:38:20 +0100

    move openapi config to shared

--- src/main/java/uk/co/bluegecko/marine/vessel/configuration/ApplicationConfiguration.java
+++ src/main/java/uk/co/bluegecko/marine/vessel/configuration/ApplicationConfiguration.java
@@ -1,13 +1,5 @@
 package uk.co.bluegecko.marine.vessel.configuration;
 
-import static org.apache.commons.text.WordUtils.capitalizeFully;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.co.bluegecko.marine.shared.configuration.SharedConfiguration;
 
@@ -16,17 +8,5 @@ import uk.co.bluegecko.marine.shared.configuration.SharedConfiguration;
  */
 @Configuration
 public class ApplicationConfiguration extends SharedConfiguration {
-
-	@Bean
-	public OpenAPI customOpenAPI(
-			@Value("${spring.application.name:Unknown Application}") String name,
-			@Value("${app.description:Unknown Description}") String description,
-			@Value("${app.version:Unknown Version}") String version,
-			@Value("${app.licence:http://localhost:8080/license.txt}") String licence) {
-		return new OpenAPI()
-				.components(new Components())
-				.info(new Info().title(capitalizeFully(name)).description(description).version(version)
-						.license(new License().name("M.I.T.").url(licence)));
-	}
 
 }
